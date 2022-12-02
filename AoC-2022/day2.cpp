c5fbc2a4af5c5aabe13918b5b1c615d32970ffc9
Perryvw <perry.wesel@gmail.com>
2022-12-02 14:56:39 +0100

    2022 day2 numerical solution

--- AoC-2022/day2.cpp
+++ AoC-2022/day2.cpp
@@ -34,52 +34,24 @@ uint32_t PointsResult(char play)
 	}
 }
 
-uint32_t Outcome(char opponentPlay, char selfPlay)
+auto OutcomeSequence(int index)
 {
-	if (selfPlay == SELF_ROCK)
-	{
-		if (opponentPlay == OPP_ROCK) return 3;
-		if (opponentPlay == OPP_PAPER) return 0;
-		if (opponentPlay == OPP_SCISSORS) return 6;
-	}
-	if (selfPlay == SELF_PAPER)
-	{
-		if (opponentPlay == OPP_ROCK) return 6;
-		if (opponentPlay == OPP_PAPER) return 3;
-		if (opponentPlay == OPP_SCISSORS) return 0;
-	}
-	if (selfPlay == SELF_SCISSORS)
-	{
-		if (opponentPlay == OPP_ROCK) return 0;
-		if (opponentPlay == OPP_PAPER) return 6;
-		if (opponentPlay == OPP_SCISSORS) return 3;
-	}
-
-	throw std::logic_error{ "Unknown round" };
+	return 1 - (index % 3);
 }
 
-char Strategy(char opponentPlay, char desiredResult)
+auto OutcomeOffset(int index)
 {
-	if (opponentPlay == OPP_ROCK)
-	{
-		if (desiredResult == RESULT_LOSE) return SELF_SCISSORS;
-		if (desiredResult == RESULT_DRAW) return SELF_ROCK;
-		if (desiredResult == RESULT_WIN) return SELF_PAPER;
-	}
-	if (opponentPlay == OPP_PAPER)
-	{
-		if (desiredResult == RESULT_LOSE) return SELF_ROCK;
-		if (desiredResult == RESULT_DRAW) return SELF_PAPER;
-		if (desiredResult == RESULT_WIN) return SELF_SCISSORS;
-	}
-	if (opponentPlay == OPP_SCISSORS)
-	{
-		if (desiredResult == RESULT_LOSE) return SELF_PAPER;
-		if (desiredResult == RESULT_DRAW) return SELF_SCISSORS;
-		if (desiredResult == RESULT_WIN) return SELF_ROCK;
-	}
+	return (4 - index) % 3;
+}
 
-	throw std::logic_error{ "Unknown round strategy" };
+auto Outcome(char opponentPlay, char selfPlay)
+{
+	return 3 + 3 * OutcomeSequence(OutcomeOffset(selfPlay - SELF_ROCK) + opponentPlay - OPP_ROCK);
+}
+
+auto Strategy(char opponentPlay, char desiredResult)
+{
+	return (opponentPlay - OPP_ROCK + desiredResult - RESULT_LOSE + 2) % 3;
 }
 
 
@@ -88,13 +60,13 @@ AOC_DAY(2)(const std::string& input)
 	std::stringstream ss{ input };
 	std::string line;
 
-	uint32_t total = 0;
-	uint32_t total2 = 0;
+	int total = 0;
+	int total2 = 0;
 
 	while (std::getline(ss, line))
 	{
 		total += Points(line[2]) + Outcome(line[0], line[2]);
-		total2 += Points(Strategy(line[0], line[2])) + PointsResult(line[2]);
+		total2 += 1 + Strategy(line[0], line[2]) + PointsResult(line[2]);
 	}
 
 	return { total, total2 };
