e4d85c1f7da45ccc28225753111999570c74ba35
Perryvw <perry.wesel@gmail.com>
2022-12-06 10:12:35 +0100

    day6

--- AoC-2022/day6.cpp
+++ AoC-2022/day6.cpp
@@ -4,11 +4,34 @@
 #include <sstream>
 #include <vector>
 
-namespace {
-
-}
-
-AOC_DAY(6)(const std::string& input)
+namespace
 {
-	return { "p1 answer", "p2 answer" };
+	auto part1(const std::string& buffer, int markerLength)
+	{
+		size_t markerStart = 0;
+		size_t markerEnd = 0;
+
+		size_t result = 0;
+
+		for (int i = 0; i < buffer.length() - 1; i++)
+		{
+			auto prevPos = std::string_view{ buffer.data() + markerStart, markerEnd + 1 - markerStart }.find(buffer[i + 1]);
+			if (prevPos != std::string::npos)
+			{
+				markerStart += prevPos + 1;
+			}
+			
+			markerEnd++;
+
+			if (1 + markerEnd - markerStart == markerLength)
+			{
+				result = markerEnd + 1;
+				break;
+			}
+		}
+
+		return result;
+	}
 }
+
+AOC_DAY(6)(const std::string& input) { return { part1(input, 4), part1(input, 14) }; }
