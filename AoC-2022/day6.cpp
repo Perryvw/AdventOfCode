2940de6c027d3da7470a1787bf38023ad1c9bb91
Perryvw <perry.wesel@gmail.com>
2022-12-06 10:17:08 +0100

    Remove obsolete var

--- AoC-2022/day6.cpp
+++ AoC-2022/day6.cpp
@@ -9,23 +9,21 @@ namespace
 	auto part1(const std::string& buffer, int markerLength)
 	{
 		size_t markerStart = 0;
-		size_t markerEnd = 0;
 
 		size_t result = 0;
 
 		for (int i = 0; i < buffer.length() - 1; i++)
 		{
-			auto prevPos = std::string_view{ buffer.data() + markerStart, markerEnd + 1 - markerStart }.find(buffer[i + 1]);
+			auto prevPos = std::string_view{ buffer.data() + markerStart, i + 1 - markerStart }.find(buffer[i + 1]);
 			if (prevPos != std::string::npos)
 			{
 				markerStart += prevPos + 1;
 			}
-			
-			markerEnd++;
 
-			if (1 + markerEnd - markerStart == markerLength)
+			// Count start and current index too, so +2
+			if (i - markerStart + 2 == markerLength)
 			{
-				result = markerEnd + 1;
+				result = i + 2;
 				break;
 			}
 		}
