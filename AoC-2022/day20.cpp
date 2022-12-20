385153a11cbc2ce0680aafd4b6533d0a8aa733f3
Perryvw <perry.wesel@gmail.com>
2022-12-20 22:40:46 +0100

    2022 day20

--- AoC-2022/day20.cpp
+++ AoC-2022/day20.cpp
@@ -6,18 +6,21 @@
 
 namespace
 {
+	constexpr size_t DECODE_KEY = 811589153;
+
+	template<typename T>
 	class RingBuffer
 	{
 	public:
 		struct Node {
 			Node* previous;
 			Node* next;
-			int value;
+			T value;
 		};
 
 		RingBuffer() { nodes.reserve(10000); }
 
-		Node& emplace_back(int v)
+		Node& emplace_back(T v)
 		{
 			if (nodes.size() == 0)
 			{
@@ -39,12 +42,12 @@ namespace
 			return last();
 		}
 
-		Node* move(RingBuffer::Node* n, int amount)
+		Node* move(RingBuffer::Node* n, int64_t amount, int64_t m)
 		{
-			auto offset = amount % static_cast<int>(size());
+			auto offset = amount % m;
 			if (offset < 0)
-				offset += size() - 1;
-			for (int x = 0; x < offset; ++x)
+				offset += m;
+			for (int64_t x = 0; x < offset; ++x)
 			{
 				n = n->next;
 			}
@@ -61,7 +64,8 @@ namespace
 		std::vector<Node> nodes;
 	};
 
-	void print(RingBuffer& rb)
+	template<typename T>
+	void print(RingBuffer<T>& rb)
 	{
 		auto* start = &rb.first();
 		auto* it = start;
@@ -76,52 +80,66 @@ namespace
 		c++;
 		std::cout << c << "/" << rb.size() << std::endl;
 	}
+
+	template<typename T>
+	void mix(RingBuffer<T>& ringbuffer)
+	{
+		for (auto& n : ringbuffer.insertOrder())
+		{
+			auto* moveAfter = ringbuffer.move(&n, n.value, ringbuffer.size() - 1);
+
+			if (moveAfter == &n)
+				continue;
+
+			auto* currentBefore = n.previous;
+			auto* currentAfter = n.next;
+			currentBefore->next = currentAfter;
+			currentAfter->previous = currentBefore;
+
+			auto* nextBefore = moveAfter;
+			auto* nextAfter = nextBefore->next;
+
+			nextBefore->next = &n;
+			nextAfter->previous = &n;
+
+			n.previous = nextBefore;
+			n.next = nextAfter;
+		}
+	}
 }
 
-FOCUS_AOC_DAY(20)(const std::string& input)
+AOC_DAY_REPS(20, 10)(const std::string& input)
 {
-	RingBuffer ringbuffer;
+	RingBuffer<int> ringbuffer;
+	RingBuffer<int64_t> ringbufferP2;
 
-	ForEachLine(input, [&](std::string_view line) { ringbuffer.emplace_back(parseInt(line)); });
+	ForEachLine(input, [&](std::string_view line) {
+		ringbuffer.emplace_back(parseInt(line));
+		ringbufferP2.emplace_back(static_cast<int64_t>(parseInt(line)) * DECODE_KEY);
+	});
 
-	// print(ringbuffer);
-
-	for (auto& n : ringbuffer.insertOrder())
-	{
-		auto offset = n.value % static_cast<int>(ringbuffer.size());
-		auto* moveAfter = ringbuffer.move(&n, offset);
-
-		if (moveAfter == &n)
-			continue;
-
-		auto* currentBefore = n.previous;
-		auto* currentAfter = n.next;
-		currentBefore->next = currentAfter;
-		currentAfter->previous = currentBefore;
-
-		auto* nextBefore = moveAfter;
-		auto* nextAfter = nextBefore->next;
-
-		nextBefore->next = &n;
-		nextAfter->previous = &n;
-
-		n.previous = nextBefore;
-		n.next = nextAfter;
-
-		// print(ringbuffer);
-	}
+	mix(ringbuffer);
 
 	auto& zero =
 		*std::find_if(ringbuffer.insertOrder().begin(), ringbuffer.insertOrder().end(), [](const auto& n) { return n.value == 0; });
-	auto* c1 = ringbuffer.move(&zero, 1000);
-	auto* c2 = ringbuffer.move(c1, 1000);
-	auto* c3 = ringbuffer.move(c2, 1000);
+	auto* c1 = ringbuffer.move(&zero, 1000, ringbuffer.size());
+	auto* c2 = ringbuffer.move(c1, 1000, ringbuffer.size());
+	auto* c3 = ringbuffer.move(c2, 1000, ringbuffer.size());
 
-	print(ringbuffer);
+	auto p1 = c1->value + c2->value + c3->value;
 
-	std::cout << c1->value << std::endl;
-	std::cout << c2->value << std::endl;
-	std::cout << c3->value << std::endl;
+	for (auto x = 0; x < 10; x++)
+	{
+		mix(ringbufferP2);
+	}
 
-	return { c1->value + c2->value + c3->value, 0 };
+	auto& zeroP2 =
+		*std::find_if(ringbufferP2.insertOrder().begin(), ringbufferP2.insertOrder().end(), [](const auto& n) { return n.value == 0; });
+	auto* c1P2 = ringbufferP2.move(&zeroP2, 1000, ringbufferP2.size());
+	auto* c2P2 = ringbufferP2.move(c1P2, 1000, ringbufferP2.size());
+	auto* c3P2 = ringbufferP2.move(c2P2, 1000, ringbufferP2.size());
+
+	auto p2 = c1P2->value + c2P2->value + c3P2->value;
+
+	return { static_cast<size_t>(p1), static_cast<size_t>(p2) };
 }
