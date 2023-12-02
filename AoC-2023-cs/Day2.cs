24b93d77d98183b23b2055fc335e902b38ddac51
Perryvw <perryvw@users.noreply.github.com>
2023-12-02 11:22:47 +0100

    2023 day2

--- AoC-2023-cs/Day2.cs
+++ AoC-2023-cs/Day2.cs
@@ -1,82 +1,77 @@
 ﻿namespace AoC2023;
 
-using Turn = (OpponentAction Opponent, YourAction You);
+using DiceAmounts = (int Red, int Green, int Blue);
 
-public enum OpponentAction
+public record Game
 {
-    Rock = 'A',
-    Paper = 'B',
-    Scissors = 'C'
+    public int Id { get; init; }
+    public List<DiceAmounts> Turns { get; init; } = null!;
 }
 
-public enum YourAction
+public partial class Day2(ITestOutputHelper output) : AoCSolution<int, int, List<Game>>(output)
 {
-    X = 'X',
-    Y = 'Y',
-    Z = 'Z'
-}
+    protected override List<Game> LoadData()
+    {
+        var lines = LoadLinesFromFile("day2.txt");
+        return lines.Select((l, i) =>
+        {
+            var turns = l.Split(":")[1];
+            return new Game
+            {
+                Id = i + 1,
+                Turns = turns.Split(";").Select(t =>
+                {
+                    var diceAmounts = t.Split(",");
+                    DiceAmounts amounts = (0, 0, 0);
 
-public class Day2(ITestOutputHelper output) : AoCSolution<int, int, List<Turn>>(output)
-{
-    protected override List<Turn> LoadData()
-        => LoadLinesFromFile("day2.txt")
-        .Select(l => ((OpponentAction)l[0], (YourAction)l[2]))
-        .ToList();
+                    foreach (var diceAmount in diceAmounts)
+                    {
+                        var dice = diceAmount.Trim().Split(" ");
+                        switch (dice[1])
+                        {
+                            case "red":
+                                amounts.Red = int.Parse(dice[0]); break;
+                            case "green":
+                                amounts.Green = int.Parse(dice[0]); break;
+                            case "blue":
+                                amounts.Blue = int.Parse(dice[0]); break;
+                        }
+                    }
+                    return amounts;
+                }).ToList()
+            };
+        }).ToList();
+    }
 
     protected override (int p1, int p2) Solve()
     {
+        DiceAmounts limitsP1 = (12, 13, 14);
+
         var p1 = Data
-            .Select(static t => ActionScore(t.You) + ResultScore(t.Opponent, t.You))
+            .Where(game => game.Turns.All(turn => turn.Red <= limitsP1.Red && turn.Green <= limitsP1.Green && turn.Blue <= limitsP1.Blue))
+            .Select(game => game.Id)
             .Sum();
 
         var p2 = Data
-            .Select(static t => ActionScore(ActionForResult(t.Opponent, t.You)) + ResultScore(t.You))
+            .Select(game => LeastPossibleDice(game))
+            .Select(leastPossibleDice => Power(leastPossibleDice))
             .Sum();
 
         return (p1, p2);
     }
 
-    private static int ActionScore(YourAction act) =>
-        act switch
+    protected static DiceAmounts LeastPossibleDice(Game game)
+    {
+        DiceAmounts amounts = (0, 0, 0);
+        foreach (var turn in game.Turns)
         {
-            YourAction.X => 1,
-            YourAction.Y => 2,
-            YourAction.Z => 3,
-        };
+            amounts.Red = Math.Max(amounts.Red, turn.Red);
+            amounts.Green = Math.Max(amounts.Green, turn.Green);
+            amounts.Blue = Math.Max(amounts.Blue, turn.Blue);
+        }
+        return amounts;
+    }
 
-    private static int ResultScore(OpponentAction opp, YourAction you) =>
-        (opp, you) switch
-        {
-            (OpponentAction.Rock, YourAction.X) => 3,
-            (OpponentAction.Rock, YourAction.Y) => 6,
-            (OpponentAction.Rock, YourAction.Z) => 0,
-            (OpponentAction.Paper, YourAction.X) => 0,
-            (OpponentAction.Paper, YourAction.Y) => 3,
-            (OpponentAction.Paper, YourAction.Z) => 6,
-            (OpponentAction.Scissors, YourAction.X) => 6,
-            (OpponentAction.Scissors, YourAction.Y) => 0,
-            (OpponentAction.Scissors, YourAction.Z) => 3,
-        };
-
-    private static int ResultScore(YourAction result) =>
-        result switch
-        {
-            YourAction.X => 0,
-            YourAction.Y => 3,
-            YourAction.Z => 6,
-        };
-
-    private static YourAction ActionForResult(OpponentAction opp, YourAction desiredResult) =>
-        (opp, desiredResult) switch
-        {
-            (OpponentAction.Rock, YourAction.X) => YourAction.Z,
-            (OpponentAction.Rock, YourAction.Y) => YourAction.X,
-            (OpponentAction.Rock, YourAction.Z) => YourAction.Y,
-            (OpponentAction.Paper, YourAction.X) => YourAction.X,
-            (OpponentAction.Paper, YourAction.Y) => YourAction.Y,
-            (OpponentAction.Paper, YourAction.Z) => YourAction.Z,
-            (OpponentAction.Scissors, YourAction.X) => YourAction.Y,
-            (OpponentAction.Scissors, YourAction.Y) => YourAction.Z,
-            (OpponentAction.Scissors, YourAction.Z) => YourAction.X,
-        };
+    protected static int Power(DiceAmounts dice) =>
+        dice.Red * dice.Green * dice.Blue;
 }
---
+++ AoC-2023-cs/Util/EnumerableExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AoC2023;
+
+internal static class EnumerableExtensions
+{
+    internal static int Product(this IEnumerable<int> items)
+        => items.Aggregate(1, (current, item) => current * item);
+}
