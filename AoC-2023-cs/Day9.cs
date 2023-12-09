20d5fd6f4771c4151a10fdbbe426b81deb5cbad8
Perryvw <perryvw@users.noreply.github.com>
2023-12-09 12:46:11 +0100

    2023 day9

---
+++ AoC-2023-cs/Day9.cs
@@ -0,0 +1,37 @@
+﻿namespace AoC2023;
+
+public partial class Day9(ITestOutputHelper output) : AoCSolution<long, long, long[][]>(output)
+{
+    protected override long[][] LoadData()
+        => LoadLinesFromFile("day9.txt")
+            .Select(l => l.Split(" ").Select(long.Parse).ToArray())
+            .ToArray();
+
+    protected override (long p1, long p2) Solve()
+    {
+        var p1 = Data
+            .Select(NextVal)
+            .Sum();
+
+        var p2 = Data
+            .Select(s => NextVal(s.Reverse()))
+            .Sum();
+
+        return (p1, p2);
+    }
+
+    protected IEnumerable<long> NextSequence(IEnumerable<long> sequence)
+    {
+        var previous = sequence.First();
+        foreach (var item in sequence.Skip(1))
+        {
+            yield return item - previous;
+            previous = item;
+        }
+    }
+
+    protected long NextVal(IEnumerable<long> sequence)
+        => sequence.All(v => v == 0)
+            ? 0
+            : sequence.Last() + NextVal(NextSequence(sequence).ToList());
+}
